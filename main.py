d228bc0c8f3dc071484a9518f817f8b932f81944
alenanicole <anjdurel@gmail.com>
2023-10-16 15:47:49 -0400

    Updated 10/16/2023

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from tkinter import filedialog
 from datetime import *
 import time
 
+import globalVars
+import rearrange_mother
+
+
 class Frame(ct.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
@@ -33,50 +37,88 @@ def switchCode(code):
         return "Paternal Grandfather -- "
     elif code == "DAU":
         return "Daughter -- "
-    
+    elif code == "SON":
+        return "Son -- "
+    elif code == "NSIS":
+        return "Sister -- "
+    elif code == "NBRO":
+        return "Brother -- " 
+    elif code == "MAUNT":
+        return "Maternal Aunt -- "
+    elif code == "PAUNT":
+        return "Paternal Aunt -- "
+    elif code == "MUNCLE":
+        return "Maternal Uncle -- "
+    elif code == "PUNCLE":
+        return "Paternal Uncle -- "
+
     return str(code)
     
-def validateName(given, family):
+def validateName(name):
     temp = ""
-
-    if str(given) != "None":
-        temp += str(given + " ")
-    
-    if str(family) != "None":
-        temp += str(family)
-
+    if str(name) != "None":
+        temp += str(name)
     return temp
     
 def upload_file():
     numOfMoth = 0
     numOfFath = 0
     file= filedialog.askopenfilename(title="Select the original patient's file", filetypes=([("XML Files","*.xml")]))
-    tree = ET.parse(file)
-    root = tree.getroot()
-    for relative in tree.findall('.//relative'):
-        code = relative.find('code')
-        codeText = code.get('code')
-        if(str(codeText) != "NMTH"):
-            numOfMoth += 1
-        if( str(codeText) != "NFTH"):
-            numOfFath += 1
-        relationshipHolder = relative.find('relationshipHolder')
-        name = relationshipHolder.find('name')
-        if(name):
-            given = name.find('given').text
-            family = name.find('family').text
-            relativeData = ""
-            relativeData += switchCode(codeText)
-            relativeData += validateName(given, family)
-            relatives.append(relativeData)
-        elif((numOfMoth != 0 and str(codeText) != "NMTH") and (numOfFath != 0 and str(codeText) != "NFTH")):
-            relativeData = ""
-            relativeData += switchCode(codeText)
-            relatives.append(relativeData)
-    output_family()
+    if file:
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for relative in tree.findall('.//relative'):
+            code = relative.find('code')
+            codeText = code.get('code')
+            if(str(codeText) == "NotAvailable"):
+                continue
+            if(str(codeText) != "NMTH"):
+                numOfMoth += 1
+            if( str(codeText) != "NFTH"):
+                numOfFath += 1
+            relationshipHolder = relative.find('relationshipHolder')
+            subjectOf1 = relative.find('subjectOf1')
+            name = relationshipHolder.find('name')
+            if(name):
+                globalVars.relativesArray.append(relative)
+                given = name.find('given').text
+                family = name.find('family').text
+                relativeData = ""
+                relativeData += switchCode(codeText)
+                relativeData += validateName(given) + " " + validateName(family)
+                relatives.append(relativeData)
+
+                codes.append(codeText)
+                first_names.append(validateName(given))
+                last_names.append(validateName(family))
+                ids.append(relationshipHolder.find('id').get('extenstion'))
+                genders.append(relationshipHolder.find('administrativeGenderCode').get('code'))
+                isDeceased = relationshipHolder.find('deceasedInd').get('value')
+                deceased.append(isDeceased)
+            elif((numOfMoth != 0 and str(codeText) != "NMTH") and (numOfFath != 0 and str(codeText) != "NFTH")):
+                globalVars.relativesArray.append(relative)
+                relativeData = ""
+                relativeData += switchCode(codeText)
+                relatives.append(relativeData)
+
+                codes.append(codeText)
+                first_names.append("")
+                last_names.append("")
+                ids.append(relationshipHolder.find('id').get('extenstion'))
+                genders.append(relationshipHolder.find('administrativeGenderCode').get('code'))
+                isDeceased = relationshipHolder.find('deceasedInd').get('value')
+                deceased.append(isDeceased)
+
+            if(subjectOf1):
+                if(isDeceased == "false"):
+                    ages.append(subjectOf1.find('livingEstimatedAge').find('value').get('value'))
+                else:
+                    ages.append(subjectOf1.find('deceasedEstimatedAge').find('value').get('value'))
+
+        output_family(tree)
             
 
-def output_family():
+def output_family(tree):
     change_to_choose_patient()
     radio_var = ct.IntVar(value=0)
     i = 0
@@ -85,10 +127,10 @@ def output_family():
         radioButton.grid(row=i, column=0, padx=20, pady=(10, 0), sticky="ew")
         i += 1
 
-    button = ct.CTkButton(master=choose_patient, text="Choose Patient", command= lambda: add_data(radio_var.get()))
+    button = ct.CTkButton(master=choose_patient, text="Choose Patient", command= lambda: add_data(radio_var.get(), tree))
     button.grid(row=i, column=0, padx=20, pady=20)
 
-def add_data(idx):
+def add_data(idx, tree):
     change_to_add_patient()
     first_name = ct.StringVar()
     last_name = ct.StringVar()
@@ -113,16 +155,15 @@ def add_data(idx):
     mrn_entry = ct.CTkEntry(master=add_patient_info, textvariable=mrn)
     mrn_entry.grid(row = 4, column = 1, padx=20, pady=(10, 0), sticky="nsew")
 
-    submit = ct.CTkButton(master=add_patient_info, text = "Submit", command= lambda: reorient_file(first_name.get(), last_name.get(), dob.get(), mrn.get()))
+    submit = ct.CTkButton(master=add_patient_info, text = "Submit", command= lambda: reorient_file(first_name.get(), last_name.get(), dob.get(), mrn.get(), idx, tree))
     submit.grid(row = 5, column = 1, padx=20, pady=(10, 0), sticky="w")
 
-def reorient_file(first_name,last_name, dob, mrn):
+
+def reorient_file(first_name,last_name, dob, mrn, idx, tree):
     change_to_loading()
     print(first_name, last_name, dob, mrn)
     ext = "117"
-    clinicName = "ClinicName"
-    instituionName = "InstitutionName"
-    gender = 'F'
+    clinicName = tree.find(".//text").text
     today = date.today()
     effective_time = today.strftime("%Y%m%d")
     today = today.strftime("%Y-%m-%d")
@@ -134,7 +175,7 @@ def reorient_file(first_name,last_name, dob, mrn):
     root = ET.Element('FamilyHistory', classCode="OBS", moodCode="EVN")
     ET.SubElement(root, 'id', root="2.16.840.1.113883.6.117", extension=ext, assigningAuthorityName="HRA" )
     ET.SubElement(root, 'code', code="10157-6",codeSystemName="LOINC", displayName="HISTORY OF FAMILY MEMBER DISEASE")
-    ET.SubElement(root, 'text').text = clinicName + "; " + instituionName
+    ET.SubElement(root, 'text').text = clinicName
     ET.SubElement(root, 'effectiveTime', value=effective_time)
     subject = ET.SubElement(root, 'subject', typeCode="SBJ")
     patient = ET.SubElement(subject, 'patient', classCode="PAT")
@@ -144,8 +185,54 @@ def reorient_file(first_name,last_name, dob, mrn):
     name = ET.SubElement(patientPerson, 'name')
     ET.SubElement(name, 'given').text = first_name
     ET.SubElement(name, 'family').text = last_name
-    ET.SubElement(patientPerson,'administrativeGenderCode', code=gender)
+    ET.SubElement(patientPerson,'administrativeGenderCode', code=genders[idx])
     ET.SubElement(patientPerson, 'birthTime', value=dob)
+    ET.SubElement(patientPerson, 'deceasedInd', value=deceased[idx])
+
+    # Break out for rearranging
+
+    #Modify Relatives
+
+    #if new patient is
+
+    # mother
+    if(str(codes[idx]) == "NMTH"):
+        rearrange_mother.rearrange(tree, patientPerson)
+    # father
+    # grandparent
+
+    # TODO: Add any subjectOf1 and subjectOf2 data for new patient
+
+
+    # # Modify extension
+    # extension = tree.find(".//id").get('extension')
+    # tree.find(".//id").set('extension', (str(int(extension) + 1)))
+    # # Modify effective time
+    # tree.find(".//effectiveTime").set('value', effective_time)
+    # # Modify the MRN
+    # patient = tree.find(".//patient")
+    # patient.find(".//id").set('extension', mrn)
+    # # Modify patient info
+    # patientPerson = tree.find(".//patientPerson")
+    # name = patientPerson.find('name')
+    # given = name.find('given')
+    # given.text = first_name
+    # family = name.find('family')
+    # family.text = last_name
+
+    # patientPerson.find('administrativeGenderCode').set('code', genders[idx])
+    # patientPerson.find('birthTime').set('value', dob)
+    # patientPerson.find('deceasedInd').set('value', deceased[idx])
+
+    # subjectOf1Info = patient.find('subjectOf1')
+    # if(deceased[idx] == 'false'):
+    #     subjectOf1Info.find('livingEstimatedAge').find('value').set('value', ages[idx])
+    #     subjectOf1Info.find('livingEstimatedAge').find('code').set('code', "21611-9")
+
+    # else:
+    #     subjectOf1Info.find('deceasedEstimatedAge').find('value').set('value', ages[idx])
+    #     subjectOf1Info.find('deceasedEstimatedAge').find('code').set('code', "39016-1")
+
 
 
 
@@ -155,8 +242,16 @@ def reorient_file(first_name,last_name, dob, mrn):
     else:
         filename = str(first_name) + " " + mrn + " HL7 " + today + "-" + current_time + ".xml"
 
+    
+    change_to_download(root, filename)
+
+   
+
+def download_file(root, filename):
     tree = ET.ElementTree(root)
-    tree.write(filename)
+    directory=filedialog.askdirectory(initialdir="/home/", title="Select a directory to download {}".format(filename))
+    if(directory):
+        tree.write(directory + "/" + filename)
 
 def change_to_upload():
     upload.grid(row=0, column=0, sticky="nsew")
@@ -188,8 +283,13 @@ def change_to_loading():
     upload.grid_forget()
     download.grid_forget()
 
-def change_to_download():
+def change_to_download(tree, filename):
     download.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+    button = ct.CTkButton(master=download, text="Download File", command= lambda: download_file(tree, filename))
+    button.grid(row=0, column=0, padx=20, pady=100)
+
+    button = ct.CTkButton(master=download, text="Quit", command=quit)
+    button.grid(row=1, column=0, padx=20, pady=100)
     add_patient_info.grid_forget()
     choose_patient.grid_forget()
     upload.grid_forget()
@@ -197,6 +297,15 @@ def change_to_download():
 
 
 relatives = []
+# Relative data paralell arrays
+first_names = []
+last_names = []
+codes = []
+genders = []
+deceased = []
+ages = []
+ids = []
+
 app = create_app()
 upload = Frame(app)
 choose_patient = Frame(app)
---
+++ rearrange_mother.py
@@ -0,0 +1,48 @@
+import globalVars
+import xml.etree.ElementTree as ET
+
+def rearrange(tree, patientPerson):
+    currentId = 8
+    for relative in globalVars.relativesArray:
+        print(relative.find('code').get('code'))
+        if((str)(relative.find('code').get('code'))== "MGRMTH"):
+            relative.find(".//code").set('code', "NMTH")
+            relationshipHolder = relative.find(".//relationshipHolder")
+            relationshipHolder.find(".//id").set('extension', "2")
+
+            relativeNew = ET.SubElement(relationshipHolder, 'relative', classCode="PRS")
+            ET.SubElement(relativeNew, 'code', code="NMTH")
+            relationshipHolderNew = ET.SubElement(relativeNew, 'relationshipHolder', classCode="PSN", determinerCode="INSTANCE")
+            ET.SubElement(relationshipHolderNew, 'id', extension="4")
+
+            relativeNew = ET.SubElement(relationshipHolder, 'relative', classCode="PRS")
+            ET.SubElement(relativeNew, 'code', code="NFTH")
+            relationshipHolderNew = ET.SubElement(relativeNew, 'relationshipHolder', classCode="PSN", determinerCode="INSTANCE")
+            ET.SubElement(relationshipHolderNew, 'id', extension="5")
+
+            patientPerson.append(relative)
+        elif((str)(relative.find('code').get('code'))== "MGRFTH"):
+            relative.find(".//code").set('code', "NFTH")
+            relationshipHolder = relative.find(".//relationshipHolder")
+            relationshipHolder.find(".//id").set('extension', "3")
+
+            relativeNew = ET.SubElement(relationshipHolder, 'relative', classCode="PRS")
+            ET.SubElement(relativeNew, 'code', code="NMTH")
+            relationshipHolderNew = ET.SubElement(relativeNew, 'relationshipHolder', classCode="PSN", determinerCode="INSTANCE")
+            ET.SubElement(relationshipHolderNew, 'id', extension="6")
+
+            relativeNew = ET.SubElement(relationshipHolder, 'relative', classCode="PRS")
+            ET.SubElement(relativeNew, 'code', code="NFTH")
+            relationshipHolderNew = ET.SubElement(relativeNew, 'relationshipHolder', classCode="PSN", determinerCode="INSTANCE")
+            ET.SubElement(relationshipHolderNew, 'id', extension="7")
+
+            patientPerson.append(relative)
+        elif((str)(relative.find(".//code").get('code')) == "NFTH"):
+            relative.find(".//code").set('code', "NotAvailable")
+            relationshipHolder = relative.find(".//relationshipHolder")
+            relationshipHolder.find(".//id").set('extension', str(currentId))
+            currentId += 1
+            for x in relationshipHolder.findall(".//relative"):
+                relationshipHolder.remove(x)
+
+            patientPerson.append(relative)
